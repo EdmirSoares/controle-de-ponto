633784c17a9e5ae0c7e7a16cd0b1dddf9f37125e
queiroz.ed <dimisoares10@gmail.com>
2024-10-19 15:07:58 -0300

    feat: header do container alteral

--- sistema-controle-ponto/src/screens/DashboardFuncionario/index.jsx
+++ sistema-controle-ponto/src/screens/DashboardFuncionario/index.jsx
@@ -1,13 +1,19 @@
 import "./styles.css";
 import SideMenu from "../../components/SideMenu";
 
-export default function DashboardFuncionario() {
+export default function DashboardFuncionario(props) {
 	return (
 		<div className="container">
 			<SideMenu />
-			<div className="rightContainer">
-				<div className="headerContainer">
-					<h1>Dashboard</h1>
+			<div className="rightContainerScreens">
+				<div className="headerContainerScreens">
+					<div className="textContentHeader">
+						<h1 className="headerTitle">{"Dashboard"}</h1>
+						<p>{"Registro de todos os pontos cadastrados"}</p>
+					</div>
+					<button className="buttonHeader">
+						{"Registrar Ponto"}
+					</button>
 				</div>
 			</div>
 		</div>
